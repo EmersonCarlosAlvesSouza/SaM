1eb793b028a998f382fbfa97ac8c55afd89f21ce
Emerson Carlos <emersoncarlosalves21@gmail.com>
2025-06-12 14:01:38 -0300

    Implementação ITOF, FTOI, FTOI e toda a parte de Instruções de manipululação da pilha

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import { SaM } from './sam.js';
 
 const samcode = [
-  "PUSH 10.5",
-  "PUSH 20.7",
-  "ADD",
+  "PUSHIMM 10",
+  "ADDSP 2",       
+  "PUSHIMM 20",    
+  "STOREOFF 1",    
+  "PUSHOFF 1",     
   "PRINT",
   "HALT"
 ];
 
 const vm = new SaM(samcode);
+vm.fbr = 0;
 vm.run();
+
--- sam.js
+++ sam.js
@@ -1,10 +1,14 @@
 export class SaM {
-  constructor(program) {
+  constructor(program, labels = {}) {
     this.stack = [];
     this.program = program;
+    this.labels = labels;
     this.pc = 0;
     this.running = true;
+    this.fbr = 0;
+
   }
+
 
   run() {
     while (this.running && this.pc < this.program.length) {
@@ -46,7 +50,7 @@ export class SaM {
       case 'TIMES':
       case 'TIMESF':
         {
-          b = this.stack.pop();
+          let b = this.stack.pop();
           vBelow = this.stack.pop();
           this.stack.push(vBelow * b);
         }
@@ -60,12 +64,12 @@ export class SaM {
         }
         break;
       case 'MOD':
-      {
-        vTop = this.stack.pop();
-        vBelow = this.stack.pop();
-        this.stack.push(vBelow % vTop);
-      }
-      break;
+        {
+          vTop = this.stack.pop();
+          vBelow = this.stack.pop();
+          this.stack.push(vBelow % vTop);
+        }
+        break;
       case 'LSHIFT':
         {
           vTop = this.stack.pop();
@@ -161,44 +165,208 @@ export class SaM {
         }
         break;
       case 'EQUAL':
-      {
-        vTop = this.stack.pop();
-        vBelow = this.stack.pop();
-        this.stack.push(vBelow === vTop ? 1 : 0);
-      }
-      break;
+        {
+          vTop = this.stack.pop();
+          vBelow = this.stack.pop();
+          this.stack.push(vBelow === vTop ? 1 : 0);
+        }
+        break;
       case 'ISNIL':
-      {
-        vTop = this.stack.pop();
-        this.stack.push(vTop === 0 ? 1 : 0);
-      }
-      break;
+        {
+          vTop = this.stack.pop();
+          this.stack.push(vTop === 0 ? 1 : 0);
+        }
+        break;
       case 'ISPOS':
-      {
-        vTop = this.stack.pop();
-        this.stack.push(vTop > 0 ? 1 : 0);
-      }
-      break;
+        {
+          vTop = this.stack.pop();
+          this.stack.push(vTop > 0 ? 1 : 0);
+        }
+        break;
       case 'ISNEG':
-      {
-        vTop = this.stack.pop();
-        this.stack.push(vTop < 0 ? 1 : 0);
-      }
-      break;
+        {
+          vTop = this.stack.pop();
+          this.stack.push(vTop < 0 ? 1 : 0);
+        }
+        break;
       case 'CMP':
       case 'CMPF':
-      {
-        vTop = this.stack.pop();
-        vBelow = this.stack.pop();
-        this.stack.push(vBelow < vTop ? -1 : vBelow > vTop ? 1 : 0);
-      }
-      break;
+        {
+          vTop = this.stack.pop();
+          vBelow = this.stack.pop();
+          this.stack.push(vBelow < vTop ? -1 : vBelow > vTop ? 1 : 0);
+        }
+        break;
       case 'PRINT':
         console.log(this.stack[this.stack.length - 1]);
         break;
       case 'HALT':
         this.running = false;
         break;
+
+      case 'ITOF':
+        {
+          vTop = this.stack.pop();
+          this.stack.push(parseFloat(vTop));
+        }
+        break;
+
+      case 'FTOI':
+        {
+          vTop = this.stack.pop();
+          this.stack.push(parseInt(vTop));
+        }
+        break;
+
+      case 'FTOIR':
+        {
+          vTop = this.stack.pop();
+          this.stack.push(Math.round(vTop));
+        }
+        break;
+      case 'PUSHIMM':
+        {
+          this.stack.push(parseInt(arg));
+        }
+        break;
+
+      case 'PUSHIMMF':
+        {
+          this.stack.push(parseFloat(arg));
+        }
+        break;
+
+      case 'PUSHIMMCH':
+        {
+          const char = arg.replace(/'/g, "");
+          this.stack.push(char.charCodeAt(0));
+        }
+        break;
+
+      case 'PUSHIMMSTR':
+        {
+          //aqui eu to simulando uma heap mano
+          if (!this.heap) {
+            this.heap = new Map();
+            this.heapPointer = 0x4000; // endereço inicial da heap (Deixei igual ao da imagem do slide do saad)
+            //a saide esta sendo 16384 porque o valor 0x4000 em decimal é 16384 caso tenha duvida mano
+          }
+          const stringValue = arg.replace(/"/g, "");
+          const address = this.heapPointer;
+          this.heap.set(address, stringValue);
+          this.stack.push(address);
+          this.heapPointer += stringValue.length; // avança o heap pointer
+        }
+        break;
+      case 'PUSHIMMPA':
+        {
+          const label = arg;
+          if (!(label in this.labels)) {
+            throw new Error(`Label ${label} não encontrado.`);
+          }
+          this.stack.push(this.labels[label]);
+        }
+        break;
+      case 'DUP':
+        {
+          vTop = this.stack[this.stack.length - 1];
+          this.stack.push(vTop);
+        }
+        break;
+
+      case 'SWAP':
+        {
+          vTop = this.stack.pop();
+          vBelow = this.stack.pop();
+          this.stack.push(vTop);
+          this.stack.push(vBelow);
+        }
+        break;
+
+
+      case 'MALLOC':
+        {
+          if (!this.heap) {
+            this.heap = new Map();
+            this.heapPointer = 0x4000;
+          }
+          const size = this.stack.pop();
+          const address = this.heapPointer;
+
+          // Alocar o bloco (simulando cada célula)
+          const allocation = new Array(size + 1);
+          allocation[0] = size; // A primeira célula guarda o tamanho
+          this.heap.set(address, allocation);
+
+          this.stack.push(address);
+          this.heapPointer += (size + 1);
+        }
+        break;
+      case 'PUSHIND':
+        {
+          const m = this.stack.pop();
+          if (m < 0 || m >= this.stack.length) {
+            throw new Error(`Endereço inválido PUSHIND: ${m}`);
+          }
+          this.stack.push(this.stack[m]);
+        }
+        break;
+
+
+      case 'STOREIND':
+        {
+          const vTop = this.stack.pop();
+          const vBelow = this.stack.pop();
+          if (vBelow < 0 || vBelow >= this.stack.length) {
+            throw new Error(`Endereço inválido STOREIND: ${vBelow}`);
+          }
+          this.stack[vBelow] = vTop;
+        }
+        break;
+
+
+      case 'ADDSP':
+        {
+          const n = parseInt(arg);
+          if (n > 0) {
+            for (let i = 0; i < n; i++) {
+              this.stack.push(0);
+            }
+          } else {
+            for (let i = 0; i < Math.abs(n); i++) {
+              if (this.stack.length === 0) break;
+              this.stack.pop();
+            }
+          }
+        }
+        break;
+
+      case 'PUSHOFF':
+        {
+          const k = parseInt(arg);
+          const address = this.fbr + k;
+          if (address < 0 || address >= this.stack.length) {
+            throw new Error(`Endereço inválido PUSHOFF: ${address}`);
+          }
+          this.stack.push(this.stack[address]);
+        }
+        break;
+
+
+      case 'STOREOFF':
+        {
+          const k = parseInt(arg);
+          const vTop = this.stack.pop();
+          const address = this.fbr + k;
+          if (address < 0 || address >= this.stack.length) {
+            throw new Error(`Endereço inválido STOREOFF: ${address}`);
+          }
+          this.stack[address] = vTop;
+        }
+        break;
+
+
+
       default:
         throw new Error(`Instrução desconhecida: ${instruction}`);
     }
